9919fe668228b7763aa61219c84dad69eefa93e3
Greeshma Sadineni <greeshma.sadineni@gmail.com>
2020-08-05 15:58:41 +0200

    Implemented Generics(Inmemoryrepositoryclass)

---
+++ MyShop/MyShop.DAtaAccess.InMemory/InMemoryRepository.cs
@@ -0,0 +1,77 @@
+﻿using MyShop.core.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.Caching;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyShop.DAtaAccess.InMemory
+{
+    public class InMemoryRepository<T> where T:BaseEntity
+    {
+        ObjectCache cache = MemoryCache.Default;
+        List<T> items;
+        string claasName;
+        public InMemoryRepository()
+        {
+            claasName = typeof(T).Name;
+            items=cache["items"] as List<T>;
+            if (items == null)
+            {
+                items = new List<T>();
+            }
+        }
+        public void Commit()
+        {
+            cache["items"] = items;
+        }
+        public void Insert(T t)
+        {
+            items.Add(t);
+        }
+        public void Update(T t)
+        {
+            T tToUpdate = items.Find(i => i.Id == t.Id);
+            if (tToUpdate != null)
+            {
+                tToUpdate = t;
+            }
+            else
+            {
+                throw new Exception(claasName + "Not found");
+            }
+        }
+        public T Find(string Id)
+        {
+            T t = items.Find(i => i.Id == Id);
+            if (t != null)
+            {
+                return t;
+            }
+            else
+            {
+                throw new Exception(claasName + "Not found");
+            }
+        }
+        public IQueryable<T> Collection()
+        {
+            return items.AsQueryable();
+        }
+        public void Delete(string Id)
+        {
+            T tToDelete = items.Find(i => i.Id == Id);
+
+            if (tToDelete != null)
+            {
+                items.Remove(tToDelete);
+            }
+            else
+            {
+                throw new Exception(claasName + "Not Found");
+            }
+        }
+
+    }
+}
